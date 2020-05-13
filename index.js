9dc19df7e3585522230e46b74d4bb216036d028d
Dmitriy Sidorov <dmitrii.sidorov@accenture.com>
2020-05-13 19:00:30 +0300

    create card

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+const fruits = [
+    {id: 1, title: 'Яблоки', price: 20, img: 'https://e1.edimdoma.ru/data/ingredients/0000/2374/2374-ed4_wide.jpg?1487746348'},
+    {id: 2, title: 'Апельсины', price: 30, img: 'https://fashion-stil.ru/wp-content/uploads/2019/04/apelsin-ispaniya-kg-92383155888981_small6.jpg'},
+    {id: 3, title: 'Манго', price: 40, img: 'https://itsfresh.ru/upload/iblock/178/178d8253202ef1c7af13bdbd67ce65cd.jpg'},
+]
+
+function createCard(options) {
+    const $card = document.createElement('div')
+    $card.classList.add('col-4')
+    $card.insertAdjacentHTML('afterbegin', `
+        <div class="card" id="${options.id}">
+            <img src="${options.img}" class="card-img-top">
+            <div class="card-body">
+                <h5 class="card-title">${options.title}</h5>
+                <a href="#" class="btn btn-primary">Price show</a>
+                <a href="#" class="btn btn-danger">Remove</a>
+            </div>
+        </div>`)
+
+    document.querySelector('[data-card-list]').appendChild($card)
+}
+
+fruits.forEach(card => createCard(card));
+
 const modal = $.modal({
     closable: true,
     content: `
@@ -22,5 +46,3 @@ const modal = $.modal({
         }
     ]
 })
-
-modal.open()
